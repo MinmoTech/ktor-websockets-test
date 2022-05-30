71f00ad50271cdd5bfffd7b26ab721f07a994435
Minmo <maroboro.mike@gmail.com>
2022-05-30 20:49:47 +0200

    Use cookie

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -3,15 +3,17 @@ package io.migaku
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.client.plugins.websocket.webSocket
+import io.ktor.client.request.cookie
 import io.ktor.http.HttpMethod
-import io.ktor.client.request.header
-import kotlinx.html.div
-import kotlinx.html.dom.append
-import org.w3c.dom.Node
+import io.ktor.util.date.GMTDate
+import io.ktor.util.date.Month
 import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.html.div
+import kotlinx.html.dom.append
+import org.w3c.dom.Node
 
 fun main() {
     window.onload = {
@@ -21,19 +23,18 @@ fun main() {
 }
 
 fun Node.sayHello() {
-    append {
-        div {
-            +"Hello from JS"
-        }
-    }
+    append { div { +"Hello from JS" } }
 }
 
 @JsExport
 fun createWebsocket() {
     GlobalScope.launch {
-        val client = HttpClient() {
-            install(WebSockets)
-        }
+        val client = HttpClient(
+            {
+                install(WebSockets) {
+                }
+            }
+        )
         println("Starting websocket")
         client.webSocket(
             method = HttpMethod.Get,
@@ -41,12 +42,21 @@ fun createWebsocket() {
             port = 8080,
             path = "/ws",
             request = {
-                header(
-                    "My-Test-Header",
-                    "My-Test-Header-Value"
+                cookie(
+                    name = "user_name",
+                    value = "jetbrains",
+                    expires =
+                    GMTDate(
+                        seconds = 0,
+                        minutes = 0,
+                        hours = 10,
+                        dayOfMonth = 1,
+                        month = Month.APRIL,
+                        year = 2023
+                    )
                 )
-            }) {
-        }
-
+            }
+        ) {}
     }
 }
+
